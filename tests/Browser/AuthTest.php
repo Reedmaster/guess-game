ce7711ae6ea11bff78007faefc5a309e46b99b0e
Alex Reed <areed22496@gmail.com>
2021-04-13 17:10:55 +0100

    added login browser test

--- tests/Browser/AuthTest.php
+++ tests/Browser/AuthTest.php
@@ -2,22 +2,40 @@
 
 namespace Tests\Browser;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Laravel\Dusk\Browser;
 use Tests\DuskTestCase;
 
 class AuthTest extends DuskTestCase
 {
+    use DatabaseMigrations;
+
     public function test_a_user_can_register_correctly()
     {
         $this->browse(function (Browser $browser) {
             $browser->visit('/register')
-                ->type('name', 'User')
-                ->type('email', 'user@user.com')
-                ->type('password', 'password')
-                ->type('password_confirmation', 'password')
-                ->press('REGISTER')
-                ->assertSee("You're logged in");
+                    ->type('name', 'User')
+                    ->type('email', 'user@user.com')
+                    ->type('password', 'password')
+                    ->type('password_confirmation', 'password')
+                    ->press('REGISTER')
+                    ->assertSee("You're logged in!")
+        });
+    }
+
+    public function test_a_user_can_login_correctly()
+    {
+        $user = User::factory()->create([
+            'email' => 'user@user.com',
+        ]);
+
+        $this->browse(function (Browser $browser) use ($user) {
+            $browser->visit('/login')
+                    ->type('email', $user->email)
+                    ->type('password', 'password')
+                    ->press('LOG IN')
+                    ->assertPathIs('/dashboard');
         });
     }
 }
